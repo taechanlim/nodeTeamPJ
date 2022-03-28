926490030b61fe39b148756a8ef77b1c81dde781
yoonk1228 <yoonk1228@naver.com>
2022-03-28 16:37:15 +0900

    feat: board/update

---
+++ src/public/js/board/update.js
@@ -0,0 +1,70 @@
+console.log('board/update 확인')
+console.log(location.pathname)
+// /board/view/9 ->
+async function view(){
+  // const [,,,idx] = location.pathname.split('/') // []
+  const [,idx] = location.href.split('=') // []
+  const intIdx = parseInt(idx)
+  console.log(intIdx)
+  let inputSubject = document.querySelector('#update_subject')
+  const nicknameBox = document.querySelector('#nickname')
+  let inputContent = document.querySelector('#update_content')
+
+  const response = await axios.post(`http://localhost:4001/api/board/view?idx=${intIdx}`,{
+    withCredentials:true,
+  })
+
+  if ( response.data.errno === 0) {
+    const [{subject,nickname,content}] = response.data.result
+    console.log(response.data)
+    inputContent.value = subject
+    nicknameBox.innerHTML = nickname
+    inputSubject.value = content
+  } else {
+
+  }
+}
+view()
+
+
+document.querySelector('#board_update_form').addEventListener('submit',async (e)=>{
+  e.preventDefault()
+  const subject = document.querySelector('#update_subject').value
+  const content = document.querySelector('#update_content').value
+  const [,idx] = location.href.split('?')
+  const [,index] = idx.split('=')
+  const intIdx = parseInt(index)
+  console.log(intIdx)
+
+
+  const body = {
+    subject:subject,
+    content:content,
+    idx:intIdx
+  }
+
+  try {
+    const response = await axios.post('http://localhost:4001/api/board/update',body,{
+      'Content-type':'application/json',
+      withCredentials:true,
+    })
+
+    if (response.data.errno !== 0) throw new Error('Error')
+    console.log(response)
+    location.href = `http://localhost:3000/board/view?idx=${intIdx}`
+  } catch (e) {
+    alert('기다려보셈; 해결해드림')
+  }
+  // response.data 담기는내용.
+  // {
+  // "result": [
+  //         {
+  //             "userid": "admin",
+  //             "name": "aa",
+  //             "nickname": "qweq",
+  //             "userlevel": 1
+  //         }
+  //     ],
+  // "errno": 0
+  // }
+})
