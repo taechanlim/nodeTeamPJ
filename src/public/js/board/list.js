e2d0900383ad30f5652b4099e7353acf703386ca
yoonk1228 <yoonk1228@naver.com>
2022-03-28 15:10:58 +0900

    feat: board/list board/view board/write

---
+++ src/public/js/board/list.js
@@ -0,0 +1,116 @@
+console.log('보드리스트 확인')
+
+let test = {}
+document.addEventListener('DOMContentLoaded',async ()=>{
+  // 요청에대한 코드를 작성해서 결과물을 받아와야합니다.
+  const response = await axios.post('http://localhost:4001/api/board/list',{
+    withCredentials:true,
+  })
+
+  // code
+  const trElement = document.querySelector('#board_row').innerHTML // String .. replace()
+  test = {
+    ...response
+  }
+  // const Nodes = response.data.result
+
+  const { total_record } = response.data // 288
+  const view_article = 10 // 한 화면에 보일 갯수
+  const block_article = 10 // 한 블럭당 보일 갯수
+
+  const total_page = Math.ceil(total_record/view_article) // 29 page
+  const total_block = Math.ceil(total_page/block_article) // 3 block
+
+  // total_page 29 1~29개
+  // total_block 3묶임
+  // current_page = 1 -> 1~10  1block
+  // current_page = 5 -> 1~10  1block
+  // current_page = 7 -> 1~10  1block
+  // current_page = 12 -> 11~20 2block
+  // current_page = 21 -> 21~29 3block
+  // current_page = 25 -> 21~29 3block
+
+  // page
+  let page = 4; // 나의 현 페이지번호를 저장하는 변수
+  const current_block = Math.ceil(page/block_article) // 1
+
+  /*
+      [1,2,3,4,5,6,7,8,9,10]          1 block
+      [11,12,13,14,15,16,17,18,19,20] 2 block
+      [21,22,23,24,25,26,27,28,29]    3 block
+  */
+  const block = ((current_block-1) * block_article )
+  /*
+      page = 7 current_block = 3
+      0 * 10 = 0 + 10 -> 0~9 -> 1~10
+      1 * 10 = 10 + 10 -> 10~19 -> 11~20
+      2 * 10 = 20 + 10 -> 30
+  */
+  let end_block = block + block_article
+  if (end_block > total_page) end_block = total_page
+
+
+  const pageing = document.querySelector('#paging > ul')
+  // 페이징 만들기
+  for(let i=block+1; i<=end_block; i++){
+    const liElement = document.createElement('li')
+    const aElement = document.createElement('a')
+    // aElement.setAttribute('href',`/board/list?page=${i}`)
+    aElement.setAttribute('onClick',`pagemove(${i})`)
+    aElement.innerHTML = `[${i}]`
+    liElement.appendChild(aElement)
+    pageing.appendChild(liElement)
+  }
+
+  // 게시물 자르기
+  const Nodes =  response.data.result.slice((page - 1) * view_article,page * view_article);
+  // splice(0,10)
+  const board_row = document.querySelector('#board_row')
+  const tbody = document.querySelector('#board tbody')
+  console.log(board_row.content)
+  Nodes.forEach(v => {
+    const clone = document.importNode(board_row.content,true)
+    const td = clone.querySelectorAll('td')
+    const aElement = document.createElement('a')
+    aElement.href = '/board/view/'+v.idx
+    aElement.innerHTML = v.subject
+
+    td[0].innerHTML = v.idx
+    td[1].appendChild(aElement)
+    td[2].innerHTML = v.nickname
+    td[3].innerHTML = v.date
+    td[4].innerHTML = v.hit
+
+    tbody.appendChild(clone)
+  })
+  // splice(0,10)
+  // let template = ''
+  // Nodes.forEach(v=>{
+  //     template += trElement.replace('{idx}',v.idx)
+  //                    .replace('{subject}',v.subject)
+  //                    .replace('{nickname}',v.nickname)
+  //                    .replace('{date}',v.date)
+  //                    .replace('{hit}',v.hit)
+  // })
+
+  // const tbody = document.querySelector('#board tbody')
+  // tbody.innerHTML = template;
+})
+
+function pagemove(num){
+  const trElement = document.querySelector('#board_row').innerHTML
+  const view_article = 10;
+  const Nodes =  test.data.result.slice((num - 1) * view_article,num * view_article);
+
+  let template = ''
+  Nodes.forEach(v=>{
+    template += trElement.replace('{idx}',v.idx)
+      .replace('{subject}',v.subject)
+      .replace('{nickname}',v.nickname)
+      .replace('{date}',v.date)
+      .replace('{hit}',v.hit)
+  })
+
+  const tbody = document.querySelector('#board tbody')
+  tbody.innerHTML = template;
+}
---
+++ src/public/js/user/login.js
@@ -0,0 +1,43 @@
+const login_frm = document.querySelector('#login_Frm_main')
+login_frm.addEventListener('submit', async (e)=>{
+  e.preventDefault()
+  // 이쪽코드가 실행된다!
+  const userid = document.querySelector('#login_userid')
+  const userpw = document.querySelector('#login_userpw')
+
+  const body = {
+    userid:userid.value,
+    userpw:userpw.value
+  }
+  try {
+    const response = await axios.post('http://localhost:4001/api/user/login',body,{
+      'Content-type':'application/json',
+      withCredentials:true,
+    })
+
+    if (response.data.errno !== 0) throw new Error('Error')
+    alert(`
+                    아이디 : ${response.data.result[0].userid}
+                    닉네임 : ${response.data.result[0].nickname}
+                `)
+    location.href = 'http://localhost:3000'
+  } catch (e) {
+    alert('아이디 와 패스워드 확인해주셈.')
+  }
+
+
+  // response.data 담기는내용.
+  //{
+  // "result": [
+  //         {
+  //             "userid": "admin",
+  //             "name": "aa",
+  //             "nickname": "qweq",
+  //             "userlevel": 1
+  //         }
+  //     ],
+  // "errno": 0
+  // }
+
+})
+
