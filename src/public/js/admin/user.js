07dc8a4bf9d2400c74fde80a785e4e3acfe0f3b5
cjh091 <cjh5701@hanmail.net>
2022-04-11 08:33:31 +0900

    정환 각종 작업

--- src/public/js/admin/user.js
+++ src/public/js/admin/user.js
@@ -1,10 +1,10 @@
 document.addEventListener('DOMContentLoaded', user)
 async function user(e){
-    const user_list = document.querySelectorAll('#user_list')
-    console.log(user_list);
+    const user_list = document.querySelectorAll('#nickname')
+    console.log(nickname);
 
 
-    const response = await axios.post('http://localhost:4001/api/user/mypageuser',{
+    const response = await axios.post('http://localhost:4001/api/admin/userinfo',{
     withCredentials:true,
     })
     const data = response.data.result;
